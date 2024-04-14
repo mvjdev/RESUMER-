8611003efa28ced1c0dc5d438a658c9a59828262
ToavinaMaria <ToavinaMaria>
2024-04-14 15:01:55 +0300

    Like.css Like.jsx

---
+++ src/components/like/Like.jsx
@@ -0,0 +1,12 @@
+import { GoHeartFill } from "react-icons/go";
+
+import React from "react";
+import "./Like.css";
+
+export default function Like() {
+  return (
+    <i className="like">
+      <GoHeartFill />
+    </i>
+  );
+}
